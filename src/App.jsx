d21d90cdb7c1e90f4cac7193bc8014ee7e2ecd23
abubakar-arman <abubakar.arman.cse@gmail.com>
2025-10-23 19:50:11 +0600

    Add Hero section with slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Footer from './components/Footer'
+import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
     <div className='bg-slate-200 h-screen'>
       <Navbar />
+      <Hero />
       <Footer />
     </div>
     </>
---
+++ src/components/Hero.jsx
@@ -0,0 +1,34 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
+
+import pet1 from '../assets/pet1.jpg'
+import pet2 from '../assets/pet2.jpg'
+import pet3 from '../assets/pet3.jpg'
+import pet4 from '../assets/pet4.jpg'
+
+const Hero = () => {
+  return (
+    <Swiper
+      modules={[Autoplay]}
+      spaceBetween={30}
+      slidesPerView={1}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+      autoplay={{delay: 3000}}
+      loop={true}
+      className='lg:h-[600px]'
+    >
+      <SwiperSlide><img src={pet1} alt="" className='object-cover w-full h-full' /></SwiperSlide>
+      <SwiperSlide><img src={pet2} alt="" className='object-cover w-full h-full' /></SwiperSlide>
+      <SwiperSlide><img src={pet3} alt="" className='object-cover w-full h-full' /></SwiperSlide>
+      <SwiperSlide><img src={pet4} alt="" className='object-cover w-full h-full' /></SwiperSlide>
+      ...
+    </Swiper>
+  );
+};
+
+export default Hero;
